4ce93c6737b225fd3a9c1af323a8d538ebad145a
Wannyon <ghkdwodnjs123@naver.com>
2024-10-23 15:45:20 +0900

    zustand 사용 예시

---
+++ src/core/presentation/pages/ZustandPractice.tsx
@@ -0,0 +1,17 @@
+import useStore from '../hooks/store'
+
+const ZustandPractice = () => {
+  const {user, updateUser} = useStore();
+  
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>Age: {user.age}</p>
+      <button onClick={() => updateUser(user.name, user.age + 1)}>
+        Increase Age
+      </button>
+    </div>
+  );
+}
+
+export default ZustandPractice;
