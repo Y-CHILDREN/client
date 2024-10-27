864d5f0991e8d81eccaaf5c394d3e348bdacdcac
Taejeong <mkiz0403@gmail.com>
2024-10-27 16:28:12 +0900

    feat : login 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ZustandPractice from './core/presentation/pages/ZustandPractice';
 import Layout from './core/presentation/components/layout/Layout';
+import Login from './core/presentation/pages/Login';
 import Home from './core/presentation/pages/Home';
 import CreateTrip from './core/presentation/pages/CreateTrip';
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Layout>
         <Router>
           <Routes>
+            <Route path="/login" element={<Login />} />
             <Route path="/zustand" element={<ZustandPractice />} />
             <Route path="/" element={<Home />} />
             <Route path="/create-trip" element={<CreateTrip />} />
