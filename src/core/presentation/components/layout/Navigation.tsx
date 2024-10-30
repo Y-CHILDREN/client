05f9d388d99c94860058f9f5487bf4b806a4fac3
Taejeong <mkiz0403@gmail.com>
2024-10-30 15:50:02 +0900

    feat : 네비게이션 레이아웃 생성

---
+++ src/core/presentation/components/layout/Navigation.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navigation: React.FC = () => {
+  return (
+    <div className="flex h-[64px] items-center flex-shrink-0 self-stretch bg-white border-t border-gray-200 ">
+      <div className="flex h-[64px] px-0 py-2 flex-col justify-center items-center gap-1 flex-[1_0_0]">
+        <img
+          src="src/core/presentation/assets/navigation/House.svg"
+          alt="home"
+          className="w-[20px] h-[20px] flex-shrink-0"
+        />
+        <div className="color-#AAADB0 text-center font-Pretendard text-[10px] font-normal leading-3">
+          홈
+        </div>
+      </div>
+      <div className="flex h-[64px] px-0 py-2 flex-col justify-center items-center gap-1 flex-[1_0_0]">
+        <img
+          src="src/core/presentation/assets/navigation/SuitcaseRolling.svg"
+          alt="내여행"
+          className="w-[20px] h-[20px] flex-shrink-0"
+        />
+        <div className="color-#AAADB0 text-center font-Pretendard text-[10px] font-normal leading-3">
+          내여행
+        </div>
+      </div>
+      <div className="flex h-[64px] px-0 py-2 flex-col justify-center items-center gap-1 flex-[1_0_0]">
+        <img
+          src="src/core/presentation/assets/navigation/User.svg"
+          alt="마이페이지"
+          className="w-[20px] h-[20px] flex-shrink-0"
+        />
+        <p className="color-#AAADB0 text-center font-Pretendard text-[10px] font-normal leading-3">
+          마이페이지
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Navigation;
