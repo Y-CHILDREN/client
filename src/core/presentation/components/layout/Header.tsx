d95f4167526ca9245b20524741b93f7612de15c7
Taejeong <mkiz0403@gmail.com>
2024-10-30 14:42:56 +0900

    refactor : css 수정

--- src/core/presentation/components/layout/Header.tsx
+++ src/core/presentation/components/layout/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <button className="menu-button p-2" />
+    <header className="flex w-[375px] h-[50px] pt-5 flex-col items-start flex-shrink-0 bg-green-500">
+      <button className="p-2 menu-button" />
     </header>
   );
 };
