7e54604f8ef60dcbc49f69b3e9bc2682d0d3efd1
Taejeong <mkiz0403@gmail.com>
2024-11-01 14:40:16 +0900

    refector :header와 footer를 삭제하고 기존 title, navigation 컴포넌트로 변경. 네이밍은 header, footer 유지

--- src/core/presentation/components/layout/Header.tsx
+++ src/core/presentation/components/layout/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  children?: React.ReactNode;
+}
+
+const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <header className="flex w-[375px] h-[50px] pt-5 flex-col items-start flex-shrink-0 bg-green-500">
-      <button className="p-2 menu-button" />
-    </header>
+    <div className="flex h-[48px] justify-center items-center flex-[1_0_0] rounded-[8px]">
+      <div className="text-center font-pretendard font-semibold text-[18px] leading-6 text-black">
+        {children}
+      </div>
+    </div>
   );
 };
 
