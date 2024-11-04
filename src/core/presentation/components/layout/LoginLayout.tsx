70692814135edffea1158682ef1a5f4ddd55213f
Taejeong <mkiz0403@gmail.com>
2024-11-04 15:51:23 +0900

    fix : 레이아웃 css 수정

--- src/core/presentation/components/layout/LoginLayout.tsx
+++ src/core/presentation/components/layout/LoginLayout.tsx
@@ -4,11 +4,9 @@ const LoginLayout: React.FC = () => {
   return (
     <>
       <div className="w-[375px] h-[812px] bg-black flex flex-col justify-btween bg-[url('src/core/presentation/assets/loginpage/loginbackground.gif')] bg-cover filter brightness-75">
-        <div className="w-[375px] h-[29px] p-[21px] bg-white"></div>
         <main className="flex flex-col items-start justify-btween p-[20px_24px_48px] flex-1 self-stretch">
           <Outlet />
         </main>
-        <div className="w-[375px] h-[21px] bg-white"></div>
       </div>
     </>
   );
