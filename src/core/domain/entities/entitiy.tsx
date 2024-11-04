e0123c48b819604543340921b7941d83ff9f6903
Taejeong <mkiz0403@gmail.com>
2024-11-04 15:50:47 +0900

    feat : User 인터페이스 정의

--- src/core/domain/entities/entitiy.tsx
+++ src/core/domain/entities/entitiy.tsx
@@ -1,1 +1,12 @@
 // 애플리케이션에서 사용되는 기본 데이터 모델을 정의하는 클래스나 타입들이 위치합니다.
+export interface User {
+  id: string;
+  provider: string;
+  email: string;
+  user_image: string;
+  nickname: string;
+  user_memo: string;
+  access_token: string;
+  refresh_token: string;
+  trip_history: [];
+}
