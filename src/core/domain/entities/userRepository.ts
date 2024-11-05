e03b2d7aa945742afd5c53184eee5dfaf0f82fba
Taejeong <mkiz0403@gmail.com>
2024-11-05 19:00:47 +0900

    feat : userRepository 생성

---
+++ src/core/domain/entities/userRepository.ts
@@ -0,0 +1,10 @@
+import User from '../entities/user';
+
+export interface UserRepository {
+  getUserById: (id: string) => Promise<User>;
+  getUserByEmail: (email: string) => Promise<User>;
+  updateUserNickname: (id: string, nickname: string) => Promise<User>;
+  updateUserMemo: (id: string, memo: string) => Promise<User>;
+  updateUserImage: (id: string, imageFile: File) => Promise<User>;
+  deleteUser: (id: string) => Promise<void>;
+}
