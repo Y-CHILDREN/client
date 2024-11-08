62c6382c81b8165c735b5d5697c201c0e610f035
Wannyon <ghkdwodnjs123@naver.com>
2024-11-08 23:34:32 +0900

    feat: Apply CSS to MultiSelectDropDown & Add Avatar

--- src/core/presentation/components/dropDown/MultiSelectDropDown.tsx
+++ src/core/presentation/components/dropDown/MultiSelectDropDown.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Check } from 'lucide-react';
+import Avatar from 'react-avatar';
 
 interface Option {
   nickname: string;
@@ -29,27 +31,44 @@ const MultiSelectDropDown: React.FC<MultiSelectDropDownProps> = ({
 
   return (
     <div
-      className={`${className} absolute top-full left-0 w-full bg-black border border-gray-300 shadow-lg z-10 
-        ${options.length > 0 ? 'max-h-40' : 'h-10'} overflow-y-auto`}
+      className={`${className} absolute top-full left-0 w-full bg-white rounded-lg border border-gray-200 shadow-lg z-10 
+        ${options.length > 0 ? 'max-h-[240px]' : 'h-[52px]'} overflow-y-auto mt-1`}
     >
       {options.length > 0 ? (
         options.map((option) => (
           <div
             key={option.email}
             onClick={() => handleClick(option.email)}
-            className="p-2 cursor-pointer hover:bg-gray-500"
+            className="flex items-center justify-between p-3 cursor-pointer hover:bg-gray-50"
           >
-            <input
-              type="checkbox"
-              checked={selectedMembers.includes(option.email)} // 이미 선택된 멤버 체크
-              readOnly
-              className="mr-2"
-            />
-            |{option.nickname}| : {option.email}
+            <div className="flex items-center gap-3">
+              <Avatar
+                src={option.avatar}
+                name={option.nickname}
+                size="32"
+                round
+                className="flex-shrink-0"
+              />
+              <span className="text-sm font-medium">{option.nickname}</span>
+            </div>
+            <div
+              className={`w-5 h-5 rounded flex items-center justify-center
+              ${
+                selectedMembers.includes(option.email)
+                  ? 'bg-[#92e7c5] text-white'
+                  : 'border-2 border-gray-200'
+              }`}
+            >
+              {selectedMembers.includes(option.email) && (
+                <Check className="h-3 w-3" />
+              )}
+            </div>
           </div>
         ))
       ) : (
-        <div className="p-2 text-gray-500">검색 결과 없음</div>
+        <div className="flex items-center justify-center h-full text-sm text-gray-500">
+          검색 결과 없음
+        </div>
       )}
     </div>
   );
