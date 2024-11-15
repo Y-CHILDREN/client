ed88881a11af223fc029fa41a3b855026bab3910
Wannyon <ghkdwodnjs123@naver.com>
2024-11-15 23:51:26 +0900

    fix: storybook arg modify at CreateTrip.stories.ts

--- src/core/presentation/components/createTrip/CreateTrip.stories.ts
+++ src/core/presentation/components/createTrip/CreateTrip.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { action } from '@storybook/addon-actions';
 
 import CrateTrip from './CreateTrip.tsx';
 
@@ -16,7 +16,6 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClose: fn() },
 } satisfies Meta<typeof CrateTrip>;
 
 export default meta;
@@ -24,5 +23,8 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Basic: Story = {
-  args: {},
+  args: {
+    onClose: action('onClose clicked'),
+    onSubmit: action('onSubmit clicked'),
+  },
 };
