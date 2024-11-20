6b2bf31b629753b0781a976b900aeb17c41610ed
Wannyon <ghkdwodnjs123@naver.com>
2024-11-21 03:36:20 +0900

    feat: 이벤트 카드 리스트 스크롤에 상태 업데이트 지연 로직 구현.

--- src/core/presentation/components/TripDetail/eventCardList/EventCardList.tsx
+++ src/core/presentation/components/TripDetail/eventCardList/EventCardList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import EventCardRow from '@/core/presentation/components/TripDetail/eventCardRow/EventCardRow.tsx';
 
 interface Cost {
@@ -35,42 +35,65 @@ const EventCardList: React.FC<Props> = ({
   onDeleteEvent,
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null); // 스크롤 지연을 줘서 상태 업데이트 빈도수를 줄인다.
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!containerRef.current) return;
-
-      const container = containerRef.current;
-      const containerCenter = container.scrollLeft + container.offsetWidth / 2;
-
-      let closestCard = null;
-      let closestDistance = Infinity;
-
-      // 모든 카드 요소를 순회하며 중앙에 가장 가까운 카드 식별
-      Array.from(container.children).forEach((child, index) => {
-        const card = child as HTMLElement;
-        const cardCenter = card.offsetLeft + card.offsetWidth / 2;
-        const distance = Math.abs(containerCenter - cardCenter);
-
-        if (distance < closestDistance) {
-          closestDistance = distance;
-          closestCard = events[index]; // 해당 이벤트 저장
-        }
-      });
-
-      // 중앙에 위치한 카드로 상태 업데이트
-      if (closestCard) {
-        setSelectedEvent(closestCard);
-      }
-    };
+  // 중앙에 가까운 카드 포커싱
+  const handleScroll = useCallback(() => {
+    if (!containerRef.current) return;
 
     const container = containerRef.current;
-    container?.addEventListener('scroll', handleScroll);
+    const containerCenter = container.scrollLeft + container.offsetWidth / 2;
 
-    return () => {
-      container?.removeEventListener('scroll', handleScroll);
-    };
+    let closestCard = null;
+    let closestDistance = Infinity;
+
+    // 모든 카드 요소를 순회하며 중앙에 가장 가까운 카드 식별
+    Array.from(container.children).forEach((child, index) => {
+      const card = child as HTMLElement;
+      const cardCenter = card.offsetLeft + card.offsetWidth / 2;
+      const distance = Math.abs(containerCenter - cardCenter);
+
+      if (distance < closestDistance) {
+        closestDistance = distance;
+        closestCard = events[index]; // 해당 이벤트 저장
+      }
+    });
+
+    // 중앙에 위치한 카드로 상태 업데이트
+    if (closestCard) {
+      setSelectedEvent(closestCard);
+    }
   }, [events, setSelectedEvent]);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (!container) return;
+
+    const debouncedScroll = () => {
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+      debounceTimeout.current = setTimeout(handleScroll, 100);
+    };
+
+    container.addEventListener('scroll', debouncedScroll);
+    return () => {
+      container.removeEventListener('scroll', debouncedScroll);
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+    };
+  }, [handleScroll]);
+
+  // 날짜 탭 이동 포커싱
+  useEffect(() => {
+    // 초기 로드 시 첫 번째 이벤트를 선택
+    if (events.length > 0) {
+      // 첫 번째 이벤트로 상태 업데이트
+      setSelectedEvent(events[0]);
+    }
+  }, [selectedDate]);
 
   return (
     <div
