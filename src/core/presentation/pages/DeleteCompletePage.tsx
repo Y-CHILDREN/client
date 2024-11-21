693124f98f2926cd147c575aaa5e78026be79753
Taejeong <mkiz0403@gmail.com>
2024-11-21 11:02:02 +0900

    feat : 회원탈퇴시 이메일을 입력받아 검증후 탈퇴 처리 하도록 구현

--- src/core/presentation/pages/DeleteCompletePage.tsx
+++ src/core/presentation/pages/DeleteCompletePage.tsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/layout/Header';
 import { useAuthStore } from '../hooks/stores/authStore';
+import { useToast } from '../hooks/useToast';
 
 const DeleteCompletePage = () => {
   const navigate = useNavigate();
-  const { clearAuth } = useAuthStore();
+  const { user, clearAuth } = useAuthStore();
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const { showToast } = useToast();
 
   const backgroundImg = '/assets/deleteCompletePage/bg_delPage.gif';
 
-  const handleConfirm = () => {
-    clearAuth();
-    navigate('/login', { replace: true });
+  const handleConfirm = async () => {
+    if (confirmEmail === user?.email) {
+      try {
+        await clearAuth();
+        navigate('/login', { replace: true });
+        showToast('탈퇴가 정상 처리 되었습니다.', 'success');
+      } catch (error) {
+        console.error(error, '회원 탈퇴중 에러발생');
+      }
+    } else {
+      showToast('이메일이 일치하지 않습니다.', 'error');
+    }
+  };
+
+  const handleCancel = () => {
+    navigate('/mypage', { replace: true });
   };
 
   return (
@@ -20,16 +37,32 @@ const DeleteCompletePage = () => {
         <div className="h-[500px] flex items-center justify-center ">
           <img src={backgroundImg} alt="byebye" />
         </div>
-        <div className="flex flex-col items-center gap-6 text-center">
-          <h2 className="text-xl font-bold">회원 탈퇴가 완료되었습니다</h2>
-          <p>ByeBye🖐️</p>
+        <div className="flex flex-col items-start gap-6 text-center">
+          <p className="px-4 text-[14px] font-bold">
+            탈퇴를 원하시면 아래 창에 이메일을 입력해주세요.
+          </p>
+          <input
+            type="text"
+            placeholder="이메일을 입력해주세요"
+            value={confirmEmail}
+            onChange={(e) => setConfirmEmail(e.target.value)}
+            className="w-full h-[48px] flex items-center p-4 border border-[#AAADB0] rounded"
+          />
         </div>
-        <button
-          onClick={handleConfirm}
-          className="mt-8 w-full max-w-[320px] h-[48px] bg-[#45E1B2] text-white rounded-lg"
-        >
-          확인
-        </button>
+        <div className="flex items-start self-stretch justify-center gap-3">
+          <button
+            onClick={handleCancel}
+            className="mt-8 w-full max-w-[320px] h-[48px] bg-[#AAADB0] text-white rounded-lg hover:cursor-pointer "
+          >
+            취소
+          </button>
+          <button
+            onClick={handleConfirm}
+            className="mt-8 w-full max-w-[320px] h-[48px] bg-[#45E1B2] text-white rounded-lg hover:cursor-pointer"
+          >
+            탈퇴하기
+          </button>
+        </div>
       </div>
     </>
   );
