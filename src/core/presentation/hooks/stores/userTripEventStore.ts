3c19679772961ef457aa9d295fb57bfff88b2e9b
Taejeong <mkiz0403@gmail.com>
2024-11-23 22:14:16 +0900

    feat: TripId state 손실을 방지하기 위해 persist 미들웨어를 사용하여 로컬 스토리지에 state 저장

--- src/core/presentation/hooks/stores/userTripEventStore.ts
+++ src/core/presentation/hooks/stores/userTripEventStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { Event } from '@/core/domain/entities/event.ts';
 
@@ -10,72 +11,97 @@ interface UserTripEventState {
   updateEventCoordinates: () => Promise<void>;
 }
 
-export const useUserTripEventStore = create<UserTripEventState>((set, get) => ({
-  selectedTripId: null,
-  setSelectedTripId: (tripId) => set({ selectedTripId: tripId }),
+export const useUserTripEventStore = create(
+  persist<UserTripEventState>(
+    (set, get) => ({
+      selectedTripId: null,
+      tripEvents: [],
 
-  tripEvents: [],
+      setSelectedTripId: (tripId) => set({ selectedTripId: tripId }),
 
-  fetchTripEvents: async (tripId: number) => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/event/all/${tripId}`,
-      );
-      if (!response.ok) {
-        throw new Error('이벤트 데이터를 불러오는데 실패했습니다.');
-      }
-      const data = await response.json();
-      set({ tripEvents: data });
-    } catch (error) {
-      console.error('이벤트 데이터 fetch 에러:', error);
-      set({ tripEvents: [] });
-    }
-  },
+      fetchTripEvents: async (tripId) => {
+        try {
+          const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/event/all/${tripId}`,
+          );
+          if (!response.ok) {
+            throw new Error('이벤트 데이터를 불러오는데 실패했습니다.');
+          }
+          const data = await response.json();
+          set({ tripEvents: data });
+        } catch (error) {
+          console.error('이벤트 데이터 fetch 에러:', error);
+          set({ tripEvents: [] });
+        }
+      },
 
-  // tripData에 좌표 추가.
-  updateEventCoordinates: async () => {
-    const { tripEvents } = get(); // 현재 상태에서 이벤트 가져오기.
-    const geocoder = new window.google.maps.Geocoder();
+      // tripData에 좌표 추가.
 
-    // 좌표 없는 이벤트 필터링.
-    const eventsWithoutCoordinates = tripEvents.filter(
-      (event) => event.latitude === undefined || event.location === undefined,
-    );
+      updateEventCoordinates: async () => {
+        const { tripEvents } = get(); // 현재 상태에서 이벤트 가져오기.
+        const geocoder = new window.google.maps.Geocoder();
 
-    // 좌표 주입.
-    if (eventsWithoutCoordinates.length > 0) {
-      const updatedEventsPromises = eventsWithoutCoordinates.map((event) => {
-        return new Promise<Event>((resolve) => {
-          geocoder.geocode({ address: event.location }, (result, status) => {
-            if (result === null) {
-              return console.log('result is null');
-            }
-
-            // result에 값이 존재하는지 확인
-            if (status === 'OK' && result[0]) {
-              const { lat, lng } = result[0].geometry.location;
-              resolve({
-                ...event,
-                latitude: lat(),
-                longitude: lng(),
-              });
-            } else {
-              console.error(`Geocoding failed for ${event.location}: `, status);
-              resolve(event); // 실패 시 원래 이벤트 반환
-            }
-          });
-        });
-      });
-
-      // Geocoding 완료 후 상태 업데이트
-      const updatedEvents = await Promise.all(updatedEventsPromises);
-
-      set({
-        tripEvents: tripEvents.map(
+        // 좌표 없는 이벤트 필터링.
+        const eventsWithoutCoordinates = tripEvents.filter(
           (event) =>
-            updatedEvents.find((e) => e.event_id === event.event_id) || event,
-        ),
-      });
-    }
-  },
-}));
+            event.latitude === undefined || event.location === undefined,
+        );
+
+        // 좌표 주입.
+        if (eventsWithoutCoordinates.length > 0) {
+          const updatedEventsPromises = eventsWithoutCoordinates.map(
+            (event) => {
+              return new Promise<Event>((resolve) => {
+                geocoder.geocode(
+                  { address: event.location },
+                  (result, status) => {
+                    if (result === null) {
+                      return console.log('result is null');
+                    }
+
+                    // result에 값이 존재하는지 확인
+                    if (status === 'OK' && result[0]) {
+                      const { lat, lng } = result[0].geometry.location;
+                      resolve({
+                        ...event,
+                        latitude: lat(),
+                        longitude: lng(),
+                      });
+                    } else {
+                      console.error(
+                        `Geocoding failed for ${event.location}: `,
+                        status,
+                      );
+                      resolve(event); // 실패 시 원래 이벤트 반환
+                    }
+                  },
+                );
+              });
+            },
+          );
+
+          // Geocoding 완료 후 상태 업데이트
+          const updatedEvents = await Promise.all(updatedEventsPromises);
+
+          set({
+            tripEvents: tripEvents.map(
+              (event) =>
+                updatedEvents.find((e) => e.event_id === event.event_id) ||
+                event,
+            ),
+          });
+        }
+      },
+    }),
+    {
+      name: 'tripId-storage',
+      partialize: (state) => ({
+        selectedTripId: state.selectedTripId,
+        tripEvents: state.tripEvents,
+        setSelectedTripId: state.setSelectedTripId,
+        fetchTripEvents: state.fetchTripEvents,
+        updateEventCoordinates: state.updateEventCoordinates,
+      }),
+    },
+  ),
+);
