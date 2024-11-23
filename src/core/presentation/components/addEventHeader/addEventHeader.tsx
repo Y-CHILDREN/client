ea72035d227a653a5373ef2e117f046af62376d6
Layton <jihwan8807@naver.com>
2024-11-24 01:33:55 +0900

    feature : event header 아이콘 navigate 구현

--- src/core/presentation/components/addEventHeader/addEventHeader.tsx
+++ src/core/presentation/components/addEventHeader/addEventHeader.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 interface AddEventHeaderProps {
   message: string;
 }
 
 const AddEventHeader: React.FC<AddEventHeaderProps> = ({ message }) => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate('/trip-detail');
+  };
+
   return (
     <section className="flex items-center h-[60px] px-3 border-b-2 border-gray-200 ">
       <img
+        onClick={navigateHandler}
         className="cursor-pointer"
         src="/assets/addEventForm/xIcon.svg"
         alt="닫기 아이콘"
