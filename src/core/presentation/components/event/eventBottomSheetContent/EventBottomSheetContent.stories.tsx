b62ecadc086fdf74c07fc3ad4e4ac0f53ac389f8
Layton <jihwan8807@naver.com>
2025-04-04 15:08:05 +0900

    feature : implement EventBottomSheetContentWrapper storybook

---
+++ src/core/presentation/components/event/eventBottomSheetContent/EventBottomSheetContent.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { EventBottomSheetContent } from './EventBottomSheetContent';
+import { useForm, FormProvider } from 'react-hook-form';
+
+const EventBottomSheetContentWrapper = () => {
+  const methods = useForm();
+
+  return (
+    <div className="max-w-md p-6 mx-auto">
+      <FormProvider {...methods}>
+        <form>
+          <EventBottomSheetContent />
+        </form>
+      </FormProvider>
+    </div>
+  );
+};
+
+const meta: Meta<typeof EventBottomSheetContentWrapper> = {
+  title: 'components/EventBottomSheetContent',
+  component: EventBottomSheetContentWrapper,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EventBottomSheetContentWrapper>;
+
+export const Default: Story = {};
