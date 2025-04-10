09937b733b37106f7bff8c97d9001c985184a361
Layton <jihwan8807@naver.com>
2025-04-10 20:11:34 +0900

    refactor : layout css 삭제후 테일윈드화

--- src/core/presentation/components/layout/Layout.tsx
+++ src/core/presentation/components/layout/Layout.tsx
@@ -9,7 +9,21 @@ const Layout: React.FC = () => {
   const location = useLocation();
   const { eventId } = useParams();
   return (
-    <div className="wrapper">
+    <div
+      className=" h-screen
+    w-[420px]
+    mx-auto
+    bg-white
+    text-black
+    flex
+    flex-col
+    relative
+    shadow-[inset_-10px_0_15px_-10px_rgba(0,0,0,0.2),_inset_10px_0_15px_-10px_rgba(0,0,0,0.2)]
+    overflow-y-hidden
+    [&::-webkit-scrollbar]:hidden
+    scrollbar-thin
+    scrollbar-none"
+    >
       {/*Main content*/}
       <main
         className={`flex-grow overflow-x-hidden overflow-y-auto ${
