12f8d8b65e39f80fb9b4a600db723f52a89fa3ce
Layton <jihwan8807@naver.com>
2025-05-25 13:15:26 +0900

    feature : PWA 적용을 위한 보일러 플레이팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 //shadcn 설치 위해 코드 추가
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  //shadcn 설치 위해 코드 추가
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['**/*'],
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico,json}'],
+        maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
