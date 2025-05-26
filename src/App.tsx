f7dd76edf54e5d7e7642a79c753a9c8046fd1532
Layton <jihwan8807@naver.com>
2025-05-26 17:40:26 +0900

    refactor : App.tsx 경로 정리

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,19 @@ import Login from './core/presentation/pages/Login';
 import Home from './core/presentation/pages/Home';
 import LoginLayout from './core/presentation/components/layout/LoginLayout';
 import Mypage from './core/presentation/pages/Mypage';
-import { CreateTripPage } from './core/presentation/pages/CreateTripPage';
-
-import { EditTripPage } from './core/presentation/pages/EditTripPage';
-
-import { HelmetProvider } from 'react-helmet-async';
-
 import Mytrips from './core/presentation/pages/Mytrips';
+import DeleteCompletePage from './core/presentation/pages/DeleteUserPage.tsx';
+import ToastMessageProvider from './core/presentation/components/ui/ToastMessageProvider.tsx';
+import EventFormSkeleton from './core/presentation/components/ui/EventFormSkeleton.tsx';
+import './core/notification/settingFCM';
+
+import { CreateTripPage } from './core/presentation/pages/CreateTripPage';
+import { EditTripPage } from './core/presentation/pages/EditTripPage';
+import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './core/presentation/components/auth/AuthProvider';
 import { ProtectedRoute } from './routers/ProtectedRouter';
-import DeleteCompletePage from './core/presentation/pages/DeleteUserPage.tsx';
 import { TripDetailPage } from './core/presentation/pages/TripDetailPage';
-import ToastMessageProvider from './core/presentation/components/ui/ToastMessageProvider.tsx';
-
 import { useGoogleMapsLoader } from '@/core/presentation/hooks/useGoogleMapsLoader.ts';
-
-import EventFormSkeleton from './core/presentation/components/ui/EventFormSkeleton.tsx';
-
 const AddEventPage = lazy(
   () => import('./core/presentation/pages/AddEventPage.tsx'),
 );
